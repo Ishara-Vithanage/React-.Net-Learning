f78dabd3ae73ebf39f86278eab62ff579907154b
Ishara-Shashantha <ishara.20244040@iit.ac.lk>
2024-11-16 21:29:31 +0530

    few modifications done

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { React, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import anandaLogo from "../assets/anandaLogo.png";
 import styles from "./styles/Login.module.css";
 
@@ -6,6 +7,13 @@ const LoginPage = () => {
     useEffect(() => {
         document.title = "Login - SAS";
     }, []);
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/dashboard");
+    }
 
     return (
         <div className={styles.loginPage}>
@@ -26,13 +34,15 @@ const LoginPage = () => {
                         type="text"
                         placeholder="User ID"
                         className={styles.inputField}
+                        required
                     />
                     <input
                         type="password"
                         placeholder="Password"
                         className={styles.inputField}
+                        required
                     />
-                    <button type="submit" className={styles.loginButton}>
+                    <button type="submit" className={styles.loginButton} onClick={handleSubmit}>
                         Login
                     </button>
                 </form>
