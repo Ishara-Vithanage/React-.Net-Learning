8c7d5850ddd9eb6ecb4e8c83fee3146ea3299e8c
Ishara-Vithanage <isharaharshana06@gmail.com>
2024-11-26 06:56:09 +0530

    sas updated. gitignore updated

---
+++ SchoolAPI/SchoolAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolAPI.Models;
+
+namespace SchoolAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<StudentInfo> StudentInfo { get; set; }
+    }
+}
---
+++ SchoolAPI/SchoolAPI/Models/StudentInfo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAPI.Models
+{
+    public class StudentInfo
+    {
+        [Key]
+        public int student_id { get; set; }
+        public string full_name { get; set; }
+    }
+}
